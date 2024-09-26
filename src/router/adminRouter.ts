d0d1c219001e16b29d6f386bcaeb98747f52aace
NaufalArkananta <naufalarkananta25@gmail.com>
2024-09-26 10:55:43 +0700

    7th commit

--- src/router/adminRouter.ts
+++ src/router/adminRouter.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { addAdmin, authentication, deleteAdmin, findAdmin, readAdmin, updateAdmin } from "../controller/adminController";
 import { addValidation, authValidation, updateValidation } from "../middleware/adminValidation";
+import { verifyToken } from "../middleware/authorization";
 
 const router = Router()
 
-router.post(`/`, [addValidation],  addAdmin)
-router.get(`/`, readAdmin)
-router.get(`/findAdmin`, findAdmin)
-router.put(`/:id`, [updateValidation], updateAdmin)
-router.delete(`/:id`, deleteAdmin)
+router.post(`/`, [verifyToken, addValidation],  addAdmin)
+router.get(`/`,[verifyToken], readAdmin)
+router.get(`/findAdmin`, [verifyToken], findAdmin)
+router.put(`/:id`, [verifyToken, updateValidation], updateAdmin)
+router.delete(`/:id`, [verifyToken], deleteAdmin)
 router.post(`/auth`, [authValidation], authentication)
 
 export default router
--- src/router/medicineRouter.ts
+++ src/router/medicineRouter.ts
@@ -2,16 +2,17 @@ import { Router } from "express";
 import { createMedicine, deleteMedicine, readMedicine, updateMedicine } from "../controller/medicineController";
 import { createValidation, updateValidation } from "../middleware/validateMedicine"
 import { uploadMedicinePhoto } from "../middleware/uploadMedicinePhoto";
+import { verifyToken } from "../middleware/authorization";
 
 const router = Router()
 
 // router for add newMedicine || single for up 1 photo
-router.post(`/`, [uploadMedicinePhoto.single(`photo`), createValidation], createMedicine)
+router.post(`/`, [verifyToken, uploadMedicinePhoto.single(`photo`), createValidation], createMedicine)
 // router for get all data
-router.get(`/`, readMedicine)
+router.get(`/`, [verifyToken], readMedicine)
 // router for update
-router.put(`/:id`, [uploadMedicinePhoto.single(`photo`), updateValidation], updateMedicine)
+router.put(`/:id`, [verifyToken, uploadMedicinePhoto.single(`photo`), updateValidation], updateMedicine)
 // router for delete
-router.delete(`/:id`, deleteMedicine)
+router.delete(`/:id`, [verifyToken], deleteMedicine)
 
 export default router
